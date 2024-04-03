0168626f37768537c01e6ce199ff25279cac2a9b
Kaio Sergio <kaioscsouza@gmail.com>
2024-04-03 20:08:53 -0300

    Commit 01 - Criando ambiente v2
    Criando ambiente v2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import { StyleSheet,} from 'react-native';
-import StackComponent from './src/routes/stack';
-
-import {
-  SafeAreaView
-} from 'react-native-safe-area-context';
-import { Provider as PaperProvider } from 'react-native-paper';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <PaperProvider>
-      <StackComponent/>
-      </PaperProvider>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
--- src/pages/home/bottom_menu/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MyBottomTabs from '../../../routes/bottomtabs';
-
-export default function Home(){
-    return(
-            <MyBottomTabs/>
-    )
-}
